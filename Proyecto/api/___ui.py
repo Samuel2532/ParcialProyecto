96c559f41e9c95beb0277823f021555da5bb06b0
Samuel2532 <samuel.parra@utp.edu.co>
2024-02-28 19:37:15 -0500

    Se actualizó el programa a una versión mas avanzada

---
+++ Proyecto/api/___ui.py
@@ -0,0 +1,16 @@
+
+
+nombre_departamento = input("Porfavor ingrese el nombre del departamento: ").upper()
+
+while True:
+
+    limite_registros = int(input("Porfavor Ingrese el número de registros que desea consultar (menores a 100): "))
+
+    if limite_registros < 100:
+        break
+
+
+            
+
+
+    
---
+++ Proyecto/api/__api.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+from sodapy import Socrata
+from ___ui import nombre_departamento, limite_registros
+from tabulate import tabulate
+
+client = Socrata ("www.datos.gov.co", None)
+
+results = client.get ("gt2j-8ykr", departamento_nom = nombre_departamento, limit = limite_registros)
+
+results_df = pd.DataFrame(results)
+
+columnas_deseadas = {'ciudad_municipio_nom': 'CIUDAD', 'departamento_nom': 'DEPARTAMENTO', 'edad': 'EDAD', 'fuente_tipo_contagio': 'FUENTE CONTAGIO', 'estado': 'ESTADO', 'ubicacion': 'UBICACION DEL CASO'}
+
+results_df = results_df[columnas_deseadas.keys()].rename(columns=columnas_deseadas)
+
+results_df = results_df.sort_values(by=['CIUDAD'])
+
+print(tabulate(results_df, headers='keys', tablefmt='outline'))
+
